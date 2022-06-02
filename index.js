791d7415039fd7d514b9d4e46936fb2ef88f77df
anshumaan24007 <anshumaan24007@gmail.com>
2022-06-03 01:05:05 +0530

    move_Zero_to_end  & leftRotate array using O(n)

--- index.js
+++ index.js
@@ -26,10 +26,50 @@ function removeDuplicates(arr){
         
     }
        
-    console.log("Remove Duplicates ",arr.splice(0,res));
+    console.log("Remove Duplicates ",arr);
 }
 
-let arr=[10,20,20,30,30,40,50,50,50];
+function moveZerotoEnd(arr){
+    let res=0;
+    for(let i=0;i<arr.length;i++)
+    {
+        if(arr[i]!=0)
+        {
+            let temp=arr[res];
+            arr[res]=arr[i];
+            arr[i]=temp;
+            res++;
+        }
+    }
+    console.log("Moved 0 in array to last",arr);
+}
+
+function leftRotate(arr,d,n){
+    console.log("left Rotate array ",arr);
+    revereseArray(arr,0,d-1);
+    revereseArray(arr,d,n-1);
+    revereseArray(arr,0,n-1);
+    
+    console.log(" "+d+" times: ",arr);
+
+
+    function revereseArray(arr,lo,hi)
+    {
+        while(lo<hi)
+        {
+            temp=arr[lo]
+            arr[lo]=arr[hi];
+            arr[hi]=temp;
+            lo++;
+            hi--;
+        }
+    }
+
+}
+
+let arr=[10,20,20,0,0,30,30,0,40,50,0,50,50];
 indexOfLargestNumber(arr);
 removeDuplicates(arr);
+moveZerotoEnd(arr);
+leftRotate(arr,d=3,arr.length);
 
